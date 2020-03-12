4a144d5915ddb219a076a845126565dc73c2db09
Alexander Pajer <alexander.pajer@integrations.at>
2020-03-12 05:08:26 +0100

    app customizer

---
+++ 09 Extensions/AppCustomizer/src/extensions/toastCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,9 @@
+declare interface IToastCustomizerApplicationCustomizerStrings {
+  Title: string;
+  FailedToLoad: string;
+}
+
+declare module "ToastCustomizerApplicationCustomizerStrings" {
+  const strings: IToastCustomizerApplicationCustomizerStrings;
+  export = strings;
+}
