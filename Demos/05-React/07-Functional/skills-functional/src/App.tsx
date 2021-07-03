abe65a88ec487b57cd00bcdc7f6e2fbb3bd41bc6
Alexander Pajer <alexander.pajer@integrations.at>
2021-07-03 11:54:19 +0200

    functional greeter

--- Demos/05-React/07-Functional/skills-functional/src/App.tsx
+++ Demos/05-React/07-Functional/skills-functional/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Greeter } from './components/greeter/greeter';
 
 function App() {
+
+  const user = { firstName: "SPFx", lastName: "Developer" };
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h2>Welcome to React</h2>
+          </div>
+          <div className="App-intro">
+            <Greeter user={user} />
+            {/* <SkillList /> */}
+          </div>
+        </div>
+    
   );
 }
 
