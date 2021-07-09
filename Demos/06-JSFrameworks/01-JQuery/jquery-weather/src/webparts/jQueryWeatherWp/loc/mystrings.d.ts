bdf8903d0448c44207dbf1afcccf2a10c9a070e4
Alexander Pajer <alexander.pajer@integrations.at>
2021-07-09 11:42:16 +0200

    jquery

---
+++ Demos/06-JSFrameworks/01-JQuery/jquery-weather/src/webparts/jQueryWeatherWp/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IJQueryWeatherWpWebPartStrings {
+    PropertyPaneDescription: string;
+    BasicGroupName: string;
+    LocationFieldLabel: string;
+}
+
+declare module 'JQueryWeatherWpWebPartStrings' {
+    const strings: IJQueryWeatherWpWebPartStrings;
+    export = strings;
+}
