feb60ce5726bcddb1c26648b28652bd25fed4d07
Alexander Pajer <alexander.pajer@integrations.at>
2021-07-09 18:35:30 +0200

    teams-tab

---
+++ Demos/09-Teams/spfx-teams-tab/src/webparts/spFxTeamsTab/components/SpFxTeamsTab.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import styles from './SpFxTeamsTab.module.scss';
+import { ISpFxTeamsTabProps } from './ISpFxTeamsTabProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+export default class SpFxTeamsTab extends React.Component<ISpFxTeamsTabProps, {}> {
+  public render(): React.ReactElement<ISpFxTeamsTabProps> {
+    let title: string = '';
+    let subTitle: string = '';
+    let siteTabTitle: string = '';
+  
+    if (this.props.context.sdks.microsoftTeams) {
+      // We have teams context for the web part
+      title = "Welcome to Teams!";
+      subTitle = "Building custom enterprise tabs for your business.";
+      siteTabTitle = "We are in the context of following Team: " + this.props.context.sdks.microsoftTeams.context.teamName;
+    }
+    else
+    {
+      // We are rendered in normal SharePoint context
+      title = "Welcome to SharePoint!";
+      subTitle = "Customize SharePoint experiences using Web Parts.";
+      siteTabTitle = "We are in the context of following site: " + this.props.context.pageContext.web.title;
+    }
+
+    return (  
+      <div className="${ styles.myFirstTeamsTab }">
+        <div className="${ styles.container }">
+          <div className="${ styles.row }">
+            <div className="${ styles.column }">
+              <span className="${ styles.title }">{title}</span>
+              <p className="${ styles.subTitle }">{subTitle}</p>
+              <p className="${ styles.description }">{siteTabTitle}</p>
+              <p className="${ styles.description }">Description property value - {escape(this.props.description)}</p>
+              <a href="https://aka.ms/spfx" className="${ styles.button }">
+                <span className="${ styles.label }">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
