679611767a1ee52227e9722460e26570d69718be
Alexander Pajer <alexander.pajer@integrations.at>
2021-07-22 14:44:33 +0200

    labs-cleanup

--- Demos/05-React/Labs/Exercise 2/react-localization/src/webparts/reactLocalization/components/IReactLocalizationProps.ts
+++
@@ -1,3 +0,0 @@
-export interface IReactLocalizationProps {
-  description: string;
-}
--- Demos/05-React/Labs/Exercise 2/react-localization/src/webparts/reactLocalization/test/ReactLocalizationWebPart.test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mocha" />
-
-import { assert } from 'chai';
-
-describe('ReactLocalizationWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
