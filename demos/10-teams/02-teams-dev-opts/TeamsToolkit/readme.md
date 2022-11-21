9f1083833d6297cf133aa0369e7222ce33a1a7d6
Alexander Pajer <alexander.pajer@integrations.at>
2022-11-21 08:51:44 +0100

    teams

--- demos/10-teams/02-teams-dev-opts/TeamsToolkit/readme.md
+++
@@ -1,9 +0,0 @@
-# Teams Toolkit
-
-- Show Scaffolding using Teams Toolkit
-- Execute
-
-  ```
-  npm i
-  npm run start
-  ```
--- demos/10-teams/03-teams-tab/ConfigurableTab/src/app/scripts/client.ts
+++
@@ -1,4 +0,0 @@
-// Automatically added for the configMathTab tab
-export * from "./configMathTab/ConfigMathTab";
-export * from "./configMathTab/ConfigMathTabConfig";
-export * from "./configMathTab/ConfigMathTabRemove";
